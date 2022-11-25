bf693e5955aa44fbb3f8c8406c0de4579220dc25
Claudia Neme <clau.neme@gmail.com>
2022-11-25 19:36:56 -0300

    CORS

--- src/index.js
+++ src/index.js
@@ -18,6 +18,13 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+  });
 
 //Routes
 app.use('/api/voter', auth, require('./routes/voter.routes'))
